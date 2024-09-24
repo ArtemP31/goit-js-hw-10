3f2b3adaa14c2a11599e953ec83373e1c45cd2b9
ArtemP31 <pysariuk2022tl31@gmail.com>
2024-09-25 00:58:22 +0300

    first job done

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -5,6 +5,20 @@ import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+
+const selector = document.querySelector('#datetime-picker');
+const buttonEl = document.querySelector('[data-start]');
+const dataDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
+
+buttonEl.addEventListener('click', startTimer);
+
+buttonEl.disabled = true;
+let intervalId = null;
+let userSelectedDate = null;
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -12,32 +26,57 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    if (selectedDates[0] <= new Date()) {
+      return iziToast.error({
+        position: 'topRight',
+
+        message: 'Please choose a date in the future',
+      });
+    }
+    userSelectedDate = selectedDates[0];
+    buttonEl.disabled = false;
   },
 };
 
-const selector = document.querySelector('.input-data');
-
 flatpickr(selector, options);
 
+
+
+function startTimer(){
+  selector.disabled = true;
+  buttonEl.disabled = true;
+
+  intervalId = setInterval(upTimer, 1000);
+  return;
+}
+
+function upTimer(){
+  const curentTime = userSelectedDate - new Date();
+  if (curentTime <= 0) {
+    selector.disabled = false;
+    clearInterval(intervalId);
+    return;
+  }
+
+
+  dataDays.textContent = String(convertMs(curentTime).days).padStart(2, 0);
+  dataHours.textContent = String(convertMs(curentTime).hours).padStart(2, 0);
+  dataMinutes.textContent = String(convertMs(curentTime).minutes).padStart(2, 0);
+  dataSeconds.textContent = String(convertMs(curentTime).seconds).padStart(2, 0);
+}
+
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
